f4d6a07bf2daaef53b497fab69f999d71056af1b
Martin Kariuki <martinkariuki7@gmail.com>
2023-07-12 15:49:00 +0200

    Added content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import { RiArrowRightDownLine } from "react-icons/ri";
+import { RiArrowRightDownLine, RiArrowRightUpLine } from "react-icons/ri";
 import Header from "./components/header/header";
 import styles from "./page.module.css";
 import Footer from "./components/footer/footer";
@@ -16,8 +16,8 @@ export default function Home() {
             <p>
               I am an experienced developer with a strong foundation in UX
               design. Throughout my career, I have successfully built numerous
-              WordPress websites and smaller web applications. I recently
-              decided to specialize in Javascript development focusing on React
+              websites and smaller web applications. I recently decided to
+              specialize in Javascript development focusing primarily on React
               and Next.js.
             </p>
 
@@ -30,45 +30,184 @@ export default function Home() {
         <section id="contact" className={styles.featuredWork}>
           <h2>Featured work</h2>
           <div className={styles.workCategory}>
-            <h3>React</h3>
+            <h3>
+              Built with <span>react</span>
+            </h3>
             <ul>
               <li>
-                <p>A replica of the English premier league application</p>
+                <p>A replica of the English premier league application.</p>
                 <div className={styles.externalLinks}>
-                  <a href="https://mk-epl.netlify.app/">Live site</a> |{" "}
-                  <a href="https://mk-epl.netlify.app/">Repo</a>
+                  <a href="https://mk-epl.netlify.app/" target="_Blank">
+                    Live site <RiArrowRightUpLine />
+                  </a>
+                  <a
+                    href="https://github.com/martinkariuki7/epl"
+                    target="_Blank"
+                    className={styles.ml3}
+                  >
+                    Repo <RiArrowRightUpLine />
+                  </a>
                 </div>
               </li>
-              <li>React 2</li>
-              <li>React 3</li>
-              <li>React 4</li>
+              <li>
+                <p>An imaginary photo studio.</p>
+                <div className={styles.externalLinks}>
+                  <a
+                    href="https://mk-react-photostudio.netlify.app/"
+                    target="_Blank"
+                  >
+                    Live site <RiArrowRightUpLine />
+                  </a>
+                  <a
+                    href="https://github.com/martinkariuki7/react-photostudio"
+                    target="_Blank"
+                    className={styles.ml3}
+                  >
+                    Repo <RiArrowRightUpLine />
+                  </a>
+                </div>
+              </li>
+              <li>
+                <p>My personal portfolio.</p>
+                <div className={styles.externalLinks}>
+                  This website :)
+                  <a
+                    href="https://github.com/martinkariuki7/portfolio"
+                    target="_Blank"
+                    className={styles.ml3}
+                  >
+                    Repo <RiArrowRightUpLine />
+                  </a>
+                </div>
+              </li>
             </ul>
           </div>
           <div className={styles.workCategory}>
-            <h3>UX design</h3>
+            <h3>
+              Built with <span>Javascript</span>
+            </h3>
             <ul>
-              <li>UX 1</li>
-              <li>UX 2</li>
-              <li>UX 3</li>
-              <li>UX 4</li>
+              <li>
+                <p>MealFinder app.</p>
+                <div className={styles.externalLinks}>
+                  <a href="https://mk-mealfinder.netlify.app/" target="_Blank">
+                    Live site <RiArrowRightUpLine />
+                  </a>
+                  <a
+                    href="https://github.com/martinkariuki7/mealfinder"
+                    target="_Blank"
+                    className={styles.ml3}
+                  >
+                    Repo <RiArrowRightUpLine />
+                  </a>
+                </div>
+              </li>
+
+              <li>
+                <p>A stylish & functional Todolist. </p>
+                <div className={styles.externalLinks}>
+                  <a href="https://mk-todolist.netlify.app/" target="_Blank">
+                    Live site <RiArrowRightUpLine />
+                  </a>
+                  <a
+                    href="https://github.com/martinkariuki7/todolist"
+                    target="_Blank"
+                    className={styles.ml3}
+                  >
+                    Repo <RiArrowRightUpLine />
+                  </a>
+                </div>
+              </li>
+              <li>
+                <p>New website for Solintegra. </p>
+                <div className={styles.externalLinks}>
+                  <a href="https://www.solintegra.no/" target="_Blank">
+                    Live site <RiArrowRightUpLine />
+                  </a>
+                </div>
+              </li>
             </ul>
           </div>
           <div className={styles.workCategory}>
-            <h3>Vanilla JS</h3>
-            <ul>
-              <li>JS 1</li>
-              <li>JS 2</li>
-              <li>JS 3</li>
-              <li>JS 4</li>
-            </ul>
-          </div>
-          <div className={styles.workCategory}>
-            <h3>WordPress</h3>
-            <ul>
-              <li>WP 1</li>
-              <li>WP 2</li>
-              <li>WP 3</li>
-              <li>WP 4</li>
+            <h3>
+              Custom <span>WordPress</span> themes
+            </h3>
+            <ul className={styles.wpThemes}>
+              <li>
+                <a href="https://fjogstad-hus.no" target="_Blank">
+                  Fjogstad hus <RiArrowRightUpLine />
+                </a>
+              </li>
+              <li>
+                <a href="https://www.safari.africa/" target="_Blank">
+                  Safari Africa <RiArrowRightUpLine />
+                </a>
+              </li>
+              <li>
+                <a href="https://www.solintegra.no/" target="_Blank">
+                  Solintegra <RiArrowRightUpLine />
+                </a>
+              </li>
+              <li>
+                <a href="https://www.hhkarosseri.no/" target="_Blank">
+                  Hellestø karosseri as <RiArrowRightUpLine />
+                </a>
+              </li>
+              <li>
+                <a href="https://www.aktivhage.no/" target="_Blank">
+                  Aktiv Hage <RiArrowRightUpLine />
+                </a>
+              </li>
+              <li>
+                <a href="https://kristoffers-hage.no/" target="_Blank">
+                  Kristoffers Hage <RiArrowRightUpLine />
+                </a>
+              </li>
+              <li>
+                <a href="https://sirdaleiendom.no/" target="_Blank">
+                  Sirdal Eiendom <RiArrowRightUpLine />
+                </a>
+              </li>
+              <li>
+                <a href="https://l91byvilla.no/" target="_Blank">
+                  L91 Byvilla <RiArrowRightUpLine />
+                </a>
+              </li>
+              <li>
+                <a href="https://daleveien77.no/" target="_Blank">
+                  Daleveien 77 <RiArrowRightUpLine />
+                </a>
+              </li>
+              <li>
+                <a href="https://jatunhoyden.no/" target="_Blank">
+                  Jåtunhøyden <RiArrowRightUpLine />
+                </a>
+              </li>
+              <li>
+                <a href="https://jatun.no/" target="_Blank">
+                  Jåtun <RiArrowRightUpLine />
+                </a>
+              </li>
+              <li>
+                <a href="https://ensign.no/" target="_Blank">
+                  Ensign <RiArrowRightUpLine />
+                </a>
+              </li>
+              <li>
+                <a href="https://vester-hagen.no/" target="_Blank">
+                  Vesterhagen <RiArrowRightUpLine />
+                </a>
+              </li>
+              <li>
+                <a href="hhttps://osterhusarena.no/" target="_Blank">
+                  Østerhus Arena <RiArrowRightUpLine />
+                </a>
+              </li>
+              <li>
+                <a href="https://toshmukei.com/" target="_Blank">
+                  Tosh Mukei portfolio <RiArrowRightUpLine />
+                </a>
+              </li>
             </ul>
           </div>
         </section>
